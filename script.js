ef4af67b9da93e0faebf4b3ad747187da354c8e6
Giovanny Gordillo <gioggonzalez214@gmail.com>
2023-06-23 16:05:38 -0500

    finished up project, now ready to launch

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialC = ["!", "@", "#", "$", "%", "&", "*", "_", "-", "?"];
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+function generatePassword() {
+  // step 1 ask for characters and limit the amount of characters
+  var userChoice = window.prompt("How many characters would you like your password to contain?");
+  userChoice = parseInt(userChoice);
+  console.log(userChoice);
 
+  if (userChoice > 20 || userChoice < 8 || !userChoice) {
+    window.alert("Must contain at least 8 characters and no more than 20.");
+    return generatePassword();
+  } //done
+
+  var possiblities = [];
+  var wantUpper = window.confirm("Click OK to include UPPERCASE letters");
+  if (wantUpper) {
+    possiblities = possiblities.concat(upperCase);
+  }
+
+  var wantLower = window.confirm("Click OK to include lowercase letters");
+  if (wantLower) {
+    possiblities = possiblities.concat(lowerCase);
+  }
+
+  var wantSpecial = window.confirm("Click OK to include Special Characters");
+  if (wantSpecial) {
+    possiblities = possiblities.concat(specialC);
+  }
+
+  var wantNumber = window.confirm("Click OK to include numbers");
+  if (wantNumber) {
+    possiblities = possiblities.concat(numbers);
+  }
+
+  console.log(possiblities);
+
+  var passWord = "";
+  for (let i = 0; i < userChoice; i++) {
+    var randomIndex = Math.floor(Math.random() * possiblities.length);
+    var randomCharacter = possiblities[randomIndex]
+    passWord += randomCharacter
+  }
+  console.log(passWord);
+  return passWord;
+}
 
 // Write password to the #password input
 function writePassword() {
